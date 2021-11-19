021d3ace0a678a02e167aa9053b400519b477a08
Anna Pittman <annaepittman@gmail.com>
2021-11-19 08:43:37 -0800

    create currency exchange class

--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import Crypto from './crypto.js';
+import CurrencyExchange from './exchange.js';
+
