1f5502d0dd30c50d1e6a37fee93f30e604b7cc18
Anna Pittman <annaepittman@gmail.com>
2021-11-19 11:11:15 -0800

    WIP

--- src/index.js
+++ src/index.js
@@ -7,34 +7,50 @@ import CurrencyExchange from './exchange.js';
 $(document).ready(function() {
   let promise = CurrencyExchange.exchange();
   $(".subButton").click(function() {
+    const input = $('#currency').val();
+    const amount = $('#amount').val();
     promise.then(function(response) {
       const body = JSON.parse(response);
-      const input = $('#currency').val();
-      const amount = $('#amount').val();
       let inputCurrency = '';
       for (let i=0; i<body.length; i++) {
         if(input === `${body[i].conversion_rates`) {
-          inputCurrency = `${body[i].conversion_rates.}`;
+          inputCurrency = `${body[i].conversion_rates}`;
         }
       } 
       let output = (amount * inputCurrency);
       $('#showExchange').show();
-      return output;
+      $('#finalAmount').text(output)
     });
   });
-});
+}); 
 
-let currencySelect = $('#currency').val();
 
-$(document).ready(function() {
+
+/*$(document).ready(function() {
   let promise = CurrencyExchange.exchange();
   $('.subButton').click(function() {
     promise.then(function(response) {
       const body = JSON.parse(response);
-      const input = $('#currency').val();
+      const currencySelect = $('#currency').val();
       const amount = $('#amount').val();
+      for(let i=0; i<body.length; i++) {
+        if (currencySelect === `${body[i].conversion_rates.EUR}`) {
+          let outputPrice = (amount * `${body[i].conversion_rates.EUR}`)
+        } else if (currencySelect === `${body[i].conversion_rates.JPY}`) {
+          let outputPrice = (amount * `${body[i].conversion_rates.JPY}`)
+        } else if (currencySelect === `${body[i].conversion_rates.GBP}`) {
+          let outputPrice = (amount * `${body[i].conversion_rates.GBP}`)
+        } else if (currencySelect === `${body[i].conversion_rates.AUD}`) {
+          let outputPrice = (amount * `${body[i].conversion_rates.AUD}`)
+        } else if (currencySelect === `${body[i].conversion_rates.CAD}`) {
+          let outputPrice = (amount * `${body[i].conversion_rates.CAD}`)
+        }
+        $('#showExchange').show();
+        $('#finalAmount').text(outputPrice);
+      }
+
 
     })
   })
-})
+}) */
 
