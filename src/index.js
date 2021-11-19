5d1e4b91f4d6e107a06f4fba67796468f9d36f8d
Anna Pittman <annaepittman@gmail.com>
2021-11-19 09:35:36 -0800

    WIP

--- src/index.js
+++ src/index.js
@@ -4,3 +4,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './exchange.js';
 
+$(document).ready(function() {
+  $(".subButton").click(function() {
+    promise.then(function(response) {
+      const body = JSON.parse(response.body);
+      const input = $('#currency').val();
+      const amount = $('#amount').val();
+      let inputCurrency = '';
+      let ouput = '';
+      let usdRate = `${conversion_rates.currency_code}`;
+      for (let i=0; i<body.length; i++) {
+        if(input === `${body[i].currency_code}`) {
+          inputCurrency = `${body[i].conversion_rates}`;
+        }
+      } 
+      output = (inputCurrency / usdCurrency)
+    })
+  })
+})
