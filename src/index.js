38ceda7f5b6d2c9f86de23bf8812c46f33fca34c
Anna Pittman <annaepittman@gmail.com>
2021-11-19 14:10:22 -0800

    WIP

--- src/index.js
+++ src/index.js
@@ -4,60 +4,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import CurrencyExchange from './exchange.js';
 
-/*$(document).ready(function() {
-  let promise = CurrencyExchange.exchange();
-  $(".subButton").click(function() {
-    promise.then(function(response) {
-      const input = $('#currency').val();
-      const amount = $('#amount').val();
-      const body = JSON.parse(response);
-      let inputCurrency = '';
-      for (let i=0; i<body.length; i++) {
-        if(input === `${body[i].conversion_rates.currency_code}`) {
-          inputCurrency = `${body[i].conversion_rates.currency_code}`;
-        };
-      }; 
-    }); 
-    function(error) {
-      $('#showErrors').text(`Unable to process request: ${error}`);
-    }
-    let output = (amount * inputCurrency);
-    $('#showExchange').show();
-    $('#finalAmount').text(output)
-  });
-});*/
- 
-
-
-
-
 $(document).ready(function() {
   let promise = CurrencyExchange.exchange();
-  $('.subButton').click(function() {
-    promise.then(function(response) {
-      const body = JSON.parse(response);
-      const currencySelect = $('#currency').val();
-      const amount = $('#amount').val();
-      let outputPrice = '';
-      for(let i=0; i<body.length; i++) {
-        if (currencySelect === `${body[i].conversion_rates.EUR}`) {
-          let outputPrice = (amount * `${body[i].conversion_rates.EUR}`)
-        } else if (currencySelect === `${body[i].conversion_rates.JPY}`) {
-          let outputPrice = (amount * `${body[i].conversion_rates.JPY}`)
-        } else if (currencySelect === `${body[i].conversion_rates.GBP}`) {
-          let outputPrice = (amount * `${body[i].conversion_rates.GBP}`)
-        } else if (currencySelect === `${body[i].conversion_rates.AUD}`) {
-          let outputPrice = (amount * `${body[i].conversion_rates.AUD}`)
-        } else if (currencySelect === `${body[i].conversion_rates.CAD}`) {
-          let outputPrice = (amount * `${body[i].conversion_rates.CAD}`)
-        }
-      };
-    });
-    function(error) {
-      $('#showErrors').text(`Unable to process request: ${error}`);
-    }
+    $('.subButton').click(function() {
+      promise.then(function(response) {
+        const body = JSON.parse(response);
+        const currencySelect = $('#currency').val();
+        const amount = $('#amount').val();
+        let outputPrice ='';
+        for(let i=0; i<body.length; i++) {
+          if (currencySelect === `${body[i].conversion_rates.EUR}`) {
+           outputPrice = (amount * `${body[i].conversion_rates.EUR}`)
+          } else if (currencySelect === `${body[i].conversion_rates.JPY}`) {
+           outputPrice = (amount * `${body[i].conversion_rates.JPY}`)
+          } else if (currencySelect === `${body[i].conversion_rates.GBP}`) {
+            outputPrice = (amount * `${body[i].conversion_rates.GBP}`)
+          } else if (currencySelect === `${body[i].conversion_rates.AUD}`) {
+            outputPrice = (amount * `${body[i].conversion_rates.AUD}`)
+          } else if (currencySelect === `${body[i].conversion_rates.CAD}`) {
+             outputPrice = (amount * `${body[i].conversion_rates.CAD}`)
+          } 
+        };
+        function(error) {
+          $('#showErrors').text(`Unable to process request: ${error}`);
+          }
+      });
     $('#showExchange').show();
     $('#finalAmount').text(outputPrice);
   });
 });
+
+
+
+
+
+
 
